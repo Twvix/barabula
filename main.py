75377aab840b9840e56a786ccc96711844c40869
Эльдар Абдулаев <166152992+Twvix@users.noreply.github.com>
2025-04-01 17:52:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+# программа с двумя экранами
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+# Экран (объект класса Screen) - это виджет типа "макет" (Screen - наследник класса RelativeLayout).
+# ScreenManager - это особый виджет, который делает видимым один из прописанных в нём экранов.
+Window.clearcolor = (0.9, 0.9, 0.9, 1)
+Window.size = (500, 600)
+
+class FirstScr(Screen):
+    def __init__(self, name='first'):
+        super().__init__(name=name) # имя экрана должно передаваться конструктору класса Screen
+        lbl = Label(text='Вs гsdasdasd', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        inpt = TextInput(multiline=False, size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3})
+
+        btn = Button(text="Прasdоasdaгasdнsadasdоsdssdaз", size_hint=(0.3, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.2})
+        btn.on_press = self.next
+        self.add_widget(btn) # экран - это виджет, на котором могут создаваться все другие (потомки)
+        self.add_widget(lbl)
+    def next(self):
+        self.manager.transition.direction = 'left' # объект класса Screen имеет свойство manager 
+                                                   # - это ссылка на родителя
+        self.manager.current = 'second'
+
+class SecondScr(Screen):
+    def __init__(self, name='second'):
+        super().__init__(name=name)
+        btn = Button(text="Вернись, вернись!")
+        btn.on_press = self.next
+        self.add_widget(btn)
+        
+    def next(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'first'
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(FirstScr())
+        sm.add_widget(SecondScr())
+        # будет показан FirstScr, потому что он добавлен первым. Это можно поменять вот так:
+        # sm.current = 'second'
+        return sm
+
+app = MyApp()
+app.run()
