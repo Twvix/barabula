3698331f824d092960d84ead277d8a200b817ac2
Эльдар Абдулаев <166152992+Twvix@users.noreply.github.com>
2025-04-05 20:27:16 +0300

    Update prognoz.py

--- prognoz.py
+++ prognoz.py
@@ -3,7 +3,7 @@ import requests
 def get_weather(city):
 
 # формируем запрос
-    url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q='+str(city)+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
     # отправляем запрос на сервер и сразу получаем результат
     weather_data = requests.get(url).json()
     # получаем данные о температуре и о том, как она ощущается
