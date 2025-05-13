d57844c66aa24d35610d3d461f2f447d1c1db723
Эльдар Абдулаев <166152992+Twvix@users.noreply.github.com>
2025-05-13 17:58:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,36 +29,41 @@ class FirstScr(Screen):
         Saint_Peterburg = Button(text="Санкт-Петербург", size_hint=(0.3, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.2})
         Moscow = Button(text="Москва", size_hint=(0.3, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.1})
         some = Button(text="Прогноз", size_hint=(0.3, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.4})
-        self.add_widget(some)
+        # self.add_widget(some)
         self.add_widget(Vyborg) # экран - это виджет, на котором могут создаваться все другие (потомки)
         self.add_widget(Saint_Peterburg)
         self.add_widget(Moscow)
-        self.add_widget(inpt)
+        # self.add_widget(inpt)
         self.add_widget(img2)
         self.add_widget(img)
         self.add_widget(lbl)
-        some.on_press = self.some
+        # some.on_press = self.some
         Vyborg.on_press = self.vbg
         Saint_Peterburg.on_press = self.spb
         Moscow.on_press = self.msc
 
-    def some(self):
-        try:
-            if inpt.text[-1] == 'ь':
-                lbl2 = Label(text=get_weather('Погода_в_'+inpt[:-1]+'и'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-                self.add_widget(lbl2)
-            elif inpt.text[-1] == 'а':
-                lbl2 = Label(text=get_weather('Погода_в_'+inpt[:-1]+'е'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-                self.add_widget(lbl2)
-            elif inpt == 'Ростов-на-Дону':
-                lbl2 = Label(text=get_weather('Погода_в_Ростове-на-Дону'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-                self.add_widget(lbl2)
-            else:
-                lbl2 = Label(text=get_weather('Погода_в_' + inpt + 'e'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-                self.add_widget(lbl2)
-        except:
-            pass
-        self.manager.current = 'some'
+    # def some(self):
+    #     self.manager.current = 'some'
+    #     print(str(inpt.text))
+    #     try:
+    #         if str(inpt.text[-1]) == 'ь':
+    #             lbl2 = Label(text=get_weather('Погода_в_'+str(inpt.text[:-1])+'и'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+    #             self.add_widget(lbl2)
+    #             print(inpt.text)
+    #         elif inpt.text[-1] == 'а':
+    #             lbl2 = Label(text=get_weather('Погода_в_'+inpt[:-1]+'е'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+    #             self.add_widget(lbl2)
+    #             print(lbl2)
+    #         elif inpt == 'Ростов-на-Дону':
+    #             lbl2 = Label(text=get_weather('Погода_в_Ростове-на-Дону'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+    #             self.add_widget(lbl2)
+    #             print(lbl2)
+    #         else:
+    #             lbl2 = Label(text=get_weather('Погода_в_' + inpt + 'e'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+    #             self.add_widget(lbl2)
+    #             print(lbl2)
+    #     except:
+    #         print('asdfsdgfdfgdsfgggggggggggggggggggggggjh')
     def vbg(self):
         # self.manager.transition.direction = 'left'
         self.manager.current = 'second'
@@ -74,9 +79,16 @@ class SecondScr(Screen):
         super().__init__(name=name)
         lbl = Label(text='Выборг', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.5}, color=(0,0,0,1))
         lbl2 = Label(text=get_weather('Погода_в_Выборге'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+        if 'дождь' in lbl2.text:
+            img3 = Image(source='pngtree-cartoon-style-rain-cloud-png-image_2486263-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img3)
+        elif 'облачно' in lbl2.text:
+            img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img)
+        else:
+            img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
+            self.add_widget(img2)
         lbl3 = Label(text=get_weather_zavtra('Погода_в_Выборге'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.25}, color=(0,0,0,1))
-        img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
-        img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
         with open('history.txt', 'a+', encoding='utf-8') as file:
             print(file.readline())
             if str(dt) in file.readline():
@@ -88,8 +100,6 @@ class SecondScr(Screen):
         file.close()
         btn = Button(text="Назад", size_hint=(0.5, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.1})
         btn.on_press = self.next
-        self.add_widget(img2)
-        self.add_widget(img)
         self.add_widget(btn)
         self.add_widget(lbl)
         self.add_widget(lbl2)
@@ -108,16 +118,21 @@ class mscScr(Screen):
         super().__init__(name=name)
         lbl = Label(text='Москва', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.5}, color=(0,0,0,1))
         lbl2 = Label(text=get_weather('Погода_в_Москве_(ВДНХ)'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-        img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
-        img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
+        if 'дождь' in lbl2.text:
+            img3 = Image(source='pngtree-cartoon-style-rain-cloud-png-image_2486263-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img3)
+        elif 'облачно' in lbl2.text:
+            img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img)
+        else:
+            img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
+            self.add_widget(img2)
         lbl3 = Label(text=get_weather_zavtra('Погода_в_Москве_(ВДНХ)'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.25}, color=(0,0,0,1))
         with open('history.txt', 'a', encoding='utf-8') as file:
             file.write('Москва' + ': ' + lbl2.text + '\n\n')
         file.close()
         btn = Button(text="Назад", size_hint=(0.5, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.1})
         btn.on_press = self.next
-        self.add_widget(img2)
-        self.add_widget(img)
         self.add_widget(btn)
         self.add_widget(lbl)
         self.add_widget(lbl2)
@@ -135,16 +150,21 @@ class spbScr(Screen):
         super().__init__(name=name)
         lbl = Label(text='Санкт-Петербург', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.5}, color=(0,0,0,1))
         lbl2 = Label(text=get_weather('Погода_в_Санкт-Петербурге'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
-        img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
-        img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
+        if 'дождь' in lbl2.text:
+            img3 = Image(source='pngtree-cartoon-style-rain-cloud-png-image_2486263-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img3)
+        elif 'облачно' in lbl2.text:
+            img = Image(source='png-transparent-dark-cloud-thumbnail-Photoroom.png', size_hint=(0.65, 0.65), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(img)
+        else:
+            img2 = Image(source='pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png', size_hint=(0.4, 0.4), pos_hint={'center_x': 0.5, 'center_y': 0.8})
+            self.add_widget(img2)
         btn = Button(text="Назад", size_hint=(0.5, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.1})
         lbl3 = Label(text=get_weather_zavtra('Погода_в_Санкт-Петербурге'), size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.25}, color=(0,0,0,1))
         with open('history.txt', 'a', encoding='utf-8') as file:
             file.write('Санкт-Петербург' + ': ' + lbl2.text + '\n\n')
         file.close()
         btn.on_press = self.next
-        self.add_widget(img2)
-        self.add_widget(img)
         self.add_widget(btn)
         self.add_widget(lbl)
         self.add_widget(lbl2)
@@ -169,8 +189,6 @@ class someScr(Screen):
         #     file.write(inpt.text + ': ' + get_weather + '\n\n')
         # file.close()
         btn.on_press = self.next
-        self.add_widget(img2)
-        self.add_widget(img)
         self.add_widget(btn)
         self.add_widget(lbl)
         
